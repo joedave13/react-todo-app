86a207333d9e36acb67b60d0690ec3829d21011b
Joshua Davian <joshuadavian@gmail.com>
2022-04-09 19:56:53 +0700

    add new todo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,38 @@
-const Header = () => {
+import { useState } from 'react';
+
+const Header = ({ setRefresh }) => {
+  const [title, setTitle] = useState('');
+
+  const addTodo = () => {
+    const newTodo = { title, done: false };
+
+    fetch('http://localhost:8000/todos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newTodo),
+    }).then(() => {
+      // When success add data, reset form with set title empty
+      setTitle('');
+      setRefresh(true);
+      setTimeout(() => {
+        alert('Todo added successfully!');
+      }, 500);
+    });
+  };
+
   return (
     <div id="todo-header" className="header">
       <h2>Simple To Do App</h2>
-      <input type="text" />
-      <span className="add-button">Add</span>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <span className="add-button" onClick={addTodo}>
+        Add
+      </span>
     </div>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,24 +1,28 @@
 import { useEffect, useState } from 'react';
 import TodoItem from './TodoItem';
 
-const TodoList = () => {
+const TodoList = ({ isRefresh, setRefresh }) => {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     // Call API to get todo data
-    fetch('http://localhost:8000/todos')
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setTodos(data);
-      })
-      .catch((err) => {
-        if (err.name === 'AbortError') {
-          console.log('Fetch Aborted');
-        }
-      });
-  }, []);
+    if (isRefresh) {
+      fetch('http://localhost:8000/todos')
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setRefresh(false);
+          setTodos(data);
+        })
+        .catch((err) => {
+          setRefresh(false);
+          if (err.name === 'AbortError') {
+            console.log('Fetch Aborted');
+          }
+        });
+    }
+  }, [isRefresh, setRefresh]);
 
   return (
     <ul id="todo-list">
